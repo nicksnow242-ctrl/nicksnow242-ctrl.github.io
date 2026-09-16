73c92a0e527fe19ac3f1992f7387201e43ef1fec
nicksnow242-ctrl <nicksnow242@gmail.com>
2026-09-15 21:08:07 -0600

    d

---
+++ Nebuluxe/playlist.js
@@ -0,0 +1,26 @@
+const playlist = [
+  {
+    src: 'Tunes/The Uncanny Valley.mp3',
+    title: 'The Uncanny Valley',
+    artist: 'Unknown Artist',
+    art: 'Tunes/AlbumArt/artwork.jpg'
+  },
+  {
+    src: 'Tunes/She Moves Like a Knife.mp3',
+    title: 'She Moves Like a Knife',
+    artist: 'Unknown Artist',
+    art: 'Tunes/AlbumArt/artwork.jpg'
+  },
+  {
+    src: 'Tunes/Simulated Stimulation.mp3',
+    title: 'Simulated Stimulation',
+    artist: 'Unknown Artist',
+    art: 'Tunes/AlbumArt/artwork (copy 2).jpg'
+  },
+  {
+    src: 'Tunes/Nightmarket.mp3',
+    title: 'Nightmarket',
+    artist: 'Unknown Artist',
+    art: 'Tunes/AlbumArt/artwork (copy 3).jpg'
+  }
+];
