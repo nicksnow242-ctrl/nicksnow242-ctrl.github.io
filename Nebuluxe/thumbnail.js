bc24fdb55cc35afc3d0e5031e186b0bc2ed467aa
nicksnow242-ctrl <nicksnow242@gmail.com>
2026-09-16 08:48:51 -0600

    Update thumbnail.js

--- Nebuluxe/thumbnail.js
+++ Nebuluxe/thumbnail.js
@@ -18,7 +18,7 @@ const thumbnails = [
     },
     {
         id: 2,
-        src: 'Images/Thumbnails/_DSC1930.png',
+        src: 'https://picsum.photos/400/600',
         alt: 'A thief came in the night',
         title: 'Divergence',
         dateCreated: '01-01-23',
