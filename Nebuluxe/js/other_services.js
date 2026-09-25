3cfeda0350d0ecf6b595d3e8f7430f14ae26147d
nicksnow242-ctrl <nicksnow242@gmail.com>
2026-09-25 14:12:54 -0600

    s

--- Nebuluxe/js/other_services.js
+++ Nebuluxe/js/other_services.js
@@ -2,8 +2,8 @@ window.other_services = [
     {
         id: 1,
         src: 'Images/other_services/test1.jpg',
-        fullsizesrc1:'Images/Fullsize/DSC1847.jpg',
-        fullsizesrc2:'Images/Fullsize/DSC1944.jpg',
+        fullsizesrc1:'Images/Fullsize/DSC18470.jpg',
+        fullsizesrc2:'Images/Fullsize/DSC19440.jpg',
         alt: 'A thief came in the night',
         name: 'Magnets',
         pricerange: '$4-$50',
@@ -13,8 +13,8 @@ window.other_services = [
     {
         id: 2,
         src: 'Images/other_services/test2.JPG',
-        fullsizesrc1:'Images/Fullsize/DSC1847.jpg',
-        fullsizesrc2:'Images/Fullsize/DSC1944.jpg',
+        fullsizesrc1:'Images/Fullsize/DSC18470.jpg',
+        fullsizesrc2:'Images/Fullsize/DSC19440.jpg',
         alt: 'A thief came in the night',
         name: 'Kits',
         pricerange: '$4-$50',
