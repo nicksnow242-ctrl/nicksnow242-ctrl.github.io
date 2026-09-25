b0b63b7dd050e7332babaaca68294af007fc8858
nicksnow242-ctrl <nicksnow242@gmail.com>
2026-09-25 12:40:15 -0600

    f

--- Nebuluxe/js/other_services.js
+++ Nebuluxe/js/other_services.js
@@ -31,5 +31,38 @@ window.other_services = [
         pricerange: '$4-$50',
         description: 'loren ipsum',
         type: 'No Collection'
+    },
+    {
+        id: 4,
+        src: 'Images/other_services/test1.jpg',
+        fullsizesrc:'Images/Images/Fullsize/DSC1847.jpg',
+        fullsizesrc2:'Images/Images/Fullsize/DSC1944.jpg',
+        alt: 'A thief came in the night',
+        name: 'Magnets',
+        pricerange: '$4-$50',
+        description: 'loren ipsum',
+        type: 'No Collection'
+    },
+    {
+        id: 5,
+        src: 'Images/other_services/test2.JPG',
+        fullsizesrc:'Images/Images/Fullsize/DSC1847.JPG',
+        fullsizesrc2:'Images/Images/Fullsize/DSC1944.jpg',
+        alt: 'A thief came in the night',
+        name: 'Kits',
+        pricerange: '$4-$50',
+        description: 'loren ipsum',
+        type: 'No Collection'
+    },
+    {
+        id: 6,
+        src: 'Images/other_services/test3.JPG',
+        fullsizesrc:'Images/Images/Fullsize/DSC1847.jpg',
+        fullsizesrc2:'Images/Images/Fullsize/DSC1944.jpg',
+        alt: 'A thief came in the night',
+        name: 'Clothing',
+        pricerange: '$4-$50',
+        description: 'loren ipsum',
+        type: 'No Collection'
     }
 ]
